742ed31c7a08be4f62af8fb0624aa07838d98ba8
congli <cong.li@inf.ethz.ch>
2022-04-07 17:45:22 +0200

    Add methods to force JIT and DEOPT

--- dalvik/src/main/java/dalvik/artemis/Artemis.java
+++ dalvik/src/main/java/dalvik/artemis/Artemis.java
@@ -1,5 +1,60 @@
 package dalvik.artemis;
 
+import java.lang.reflect.Method;
+
+/**
+ * Utility class for Artemis
+ */
 public class Artemis {
-  public static native boolean helloJniArtemis();
+    /**
+     * Return whehter JIT is enabled in ART.
+     */
+    public static native boolean isJitEnabled();
+
+    /**
+     * Return whether the method being invoked (caller of isJitCompiled()) is JIT compiled.
+     */
+    public static native boolean isJitCompiled();
+
+    /**
+     * Return whether the given method is JIT compiled.
+     */
+    public static native boolean isMethodJitCompiled(Method method);
+
+    /**
+     * Return whether the given method is JIT compiled.
+     */
+    public static boolean isMethodJitCompiled(Class<?> clazz, 
+                                              String methodName, 
+                                              Class<?>... args) throws NoSuchMethodException, SecurityException {
+        return isMethodJitCompiled(clazz.getDeclaredMethod(methodName, args));
+    }
+
+    /**
+     * Force to JIT compile a given method. Return whether the method is successfully JIT compiled.
+     */
+    public static native boolean ensureMethodJitCompiled(Method method);
+
+    /**
+     * Force to JIT compile a given method. Return whether the method is successfully JIT compiled.
+     */
+    public static boolean ensureMethodJitCompiled(Class<?> clazz, 
+                                                  String methodName, 
+                                                  Class<?>... args) throws NoSuchMethodException, SecurityException {
+        return ensureMethodJitCompiled(clazz.getDeclaredMethod(methodName, args));
+    }
+
+    /**
+     * Force to depotimize a given method. Return whether the method is successfully depotimized.
+     */
+    public static native boolean ensureMethodDeoptimized(Method method);
+
+    /**
+     * Force to depotimize a given method. Return whether the method is successfully JIT depotimized.
+     */
+    public static boolean ensureMethodDeoptimized(Class<?> clazz, 
+                                                  String methodName, 
+                                                  Class<?>... args) throws NoSuchMethodException, SecurityException {
+        return ensureMethodDeoptimized(clazz.getDeclaredMethod(methodName, args));
+    }
 }
